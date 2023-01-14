b19c837da9e3e7a5b50fa11b79b69c408d67476b
depeche00777 <99623896+depeche00777@users.noreply.github.com>
2023-01-14 23:38:21 +0900

    찐 Api이용

--- src/context/YoutubeApiContext.js
+++ src/context/YoutubeApiContext.js
@@ -1,13 +1,13 @@
 //리팩토링 해준 이후
 import { createContext, useContext } from "react";
-import FakeYoutubeClient from "../api/fakeYoutubeClient"; //저장해놓은 파일 사용시
-//import YoutubeClient from "../api/youtubeClient"; //찐 사용
+// import FakeYoutubeClient from "../api/fakeYoutubeClient"; //저장해놓은 파일 사용시
+import YoutubeClient from "../api/youtubeClient"; //찐 사용
 import Youtube from "../api/youtube";
 
 export const YoutubeApiContext = createContext();
 
-const client = new FakeYoutubeClient(); //저장해놓은 파일 사용시
-// const client = new YoutubeClient(); //찐 사용
+// const client = new FakeYoutubeClient(); //저장해놓은 파일 사용시
+const client = new YoutubeClient(); //찐 사용
 const youtube = new Youtube(client);
 
 export function YoutubeApiProvider({ children }) {
